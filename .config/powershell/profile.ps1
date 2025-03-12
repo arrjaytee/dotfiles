47bc98c14c11f952f4ee58d2ead6ddcef8639dad
Rob Thatcher <rob.thatcher@bp.com>
2025-03-12 10:40:47 +0000

    wip: stuff

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,5 +1,32 @@
 $Env:PY_COLORS = "1"
 
+$Env:PIP_NO_PYTHON_VERSION_WARNING = "1"
+$Env:PIP_DISABLE_PIP_VERSION_CHECK = "1"
+
+# TODO we want automatic .venv/ activation
+
+If ($IsWindows) {
+    $Env:HOME = "$Env:USERPROFILE"
+}
+
+function pt {
+    & pytest --stepwise --maxfail 1 -s -l -rN --tb=native --log-cli-level=DEBUG --log-level=ERROR -p no:cov --disable-warnings
+}
+
+function act {
+    .venv/Scripts/activate.ps1 || conda activate .venv/
+}
+
+
+function deact {
+    if (Test-Path Env:VIRTUAL_ENV) {
+        deactivate
+    }
+    else {
+        conda deactivate
+    }
+}
+
 function Get-Process-From-Prefix {
     [CmdletBinding()]param(
         [string]$Prefix = $Env:CONDA_PREFIX
@@ -9,6 +36,47 @@ function Get-Process-From-Prefix {
 
 Set-Alias trash Remove-ItemSafely -Option AllScope
 
+function Install-My-Stuff {
+    Install-Module -Name Recycle -RequiredVersion 1.0.2
+}
+
+
+function rmrf {
+    [CmdletBinding()]
+    param(
+        [string]$Path
+    )
+    Remove-Item -Force -Recurse -Path $Path
+}
+
+function tox3 {
+    uvx --from "tox<4" tox @Args
+}
+
+function tox4 {
+    uvx --from "tox>=4" tox @Args
+}
+function tox4uv {
+    uvx --from "tox>=4" --with tox-uv tox @Args
+}
+
+function cfg {
+    git --git-dir $Env:HOME/.cfg --work-tree=$Env:HOME $Args
+}
+
+function excel {
+    $Excel = "$Env:ProgramFiles\Microsoft Office\Root\Office16\EXCEL.EXE"
+    Start-Process -FilePath $Excel -ArgumentList @Args
+}
+
+function setx {
+    [CmdletBinding()]
+    param(
+        [string]$name,
+        [string]$value
+    )
+    [System.Environment]::SetEnvironmentVariable($name, $value, [System.EnvironmentVariableTarget]::User)
+}
 
 function which {
     [CmdletBinding()]
@@ -18,7 +86,7 @@ function which {
 
 function whois {
     [CmdletBinding()] param($userid)
-    ([adsisearcher]"(samaccountname=$userid)").FindOne().Properties | Format-Table
+    [adsisearcher]"(samaccountname=$userid)".FindOne().Properties | Format-Table
 }
 
 function findfolk {
